0e1a9f7bef12d95aba1c080cdde8a3da0f824574
humhei <humhei@outlook.com>
2019-03-10 17:00:16 +0800

    Update solution

--- src/FsLive.Cli/fslive.fs
+++ src/FsLive.Cli/fslive.fs
@@ -1,28 +1,2 @@
-﻿// Copyright 2018 Fabulous contributors. See LICENSE.md for license.
+﻿module File1   
 
-// F# Compiler Daemon sample
-//
-// Sample use, assumes app has a reference to ELmish.XamrinForms.LiveUpdate:
-//
-// cd Fabulous\Samples\CounterApp\CounterApp
-//   adb -d forward  tcp:9867 tcp:9867
-// dotnet run --project ..\..\..\Fabulous.Cli\Fabulous.Cli.fsproj -- --eval @out.args
-// dotnet run --project ..\..\..\Fabulous.Cli\Fabulous.Cli.fsproj -- --watch --webhook:http://localhost:9867/update @out.args
-
-module FsLive.Driver
-
-open FsLive.Porta.ProcessCommandLine
-open System
-
-
-#if !TEST
-[<EntryPoint>]
-#endif
-let main (argv: string[]) =
-    try 
-        System.Environment.SetEnvironmentVariable("LIVECHECK", "1")
-        ProcessCommandLine argv
-
-    with e -> 
-        printfn "Error: %s" (e.ToString())
-        1
